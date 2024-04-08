025793cd92a7839aa2ce9250bc71b20c81ce01f9
Mutnalsaksha <mutnalsaksha@gmail.com>
2024-04-08 15:30:29 +0530

    Added environment folder

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://rmsadmin-backend-2.onrender.com', // Replace with your hosted backend URL
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    apiUrl: 'https://rmsadmin-backend-2.onrender.com', // Replace with your local backend URL during development
+};
